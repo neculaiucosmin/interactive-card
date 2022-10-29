d71672222d69706cab9569e131426aac98b5e7b3
WatafacRumeinia <cosminflorinel23@gmail.com>
2022-10-29 20:49:25 +0300

    CSS update

--- src/components/FormComponent.vue
+++ src/components/FormComponent.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="page">
     <CardComponent
-        :cvc="CVC"
-        :full_name="full_name"
-        :MM="MM" :YY="YY"/>
+        :cvc="card_details.CVC"
+        :full_name="card_details.full_name"
+        :MM="card_details.MM" :YY="card_details.YY"/>
     <div class="form-wrapper">
-      <form>
+      <form v-if="validInput">
         <div class="card-info">
           <label for="card-holder">CARDHOLDER NAME</label><br>
-          <input type="text" id="card-holder" placeholder="e.g Jane Appleseed" required v-model="full_name"
+          <input type="text" id="card-holder" placeholder="e.g Jane Appleseed" required v-model="card_details.full_name"
                  maxlength="50">
         </div>
         <div class="card-info">
           <label for="card-number">CARD NUMBER</label><br>
-          <input type="text" id="card-number" placeholder="e.g 1234 5678 9123 0000" required maxlength="16">
+          <input type="number" id="card-number" placeholder="e.g 1234 5678 9123 0000" required maxlength="16" :class="{error:errors.cardNumberError}">
         </div>
         <div class="date-cvv">
           <div class="labels">
@@ -21,32 +21,68 @@
             <label for="cvc">CVC</label>
           </div>
           <div class="input-wrapper">
-            <input type="text" id="exp-date" placeholder="MM" required maxlength="2" v-model="MM">
-            <input type="text" placeholder="YY" required maxlength="2" v-model="YY">
-            <input type="text" id="cvc" placeholder="123" maxlength="3" v-model="CVC" required>
+            <input type="number" id="exp-date" placeholder="MM" required maxlength="2" v-model="card_details.MM"
+                   :class="{error: errors.dateError}">
+
+            <input type="number" placeholder="YY" required maxlength="2" v-model="card_details.YY"
+                   :class="{error: errors.dateError}">
+            <input type="number" id="cvc" placeholder="123" maxlength="3" v-model="card_details.CVC" required
+                   :class="{error:errors.CVCError}">
           </div>
         </div>
-        <button id="btn">Confirm</button>
+        <button type="button" id="btn" @click="validation">Confirm</button>
       </form>
+      <div class="complete-state" v-else>
+        <img src="../assets/icon-complete.svg" alt="Complete">
+        <h1>THANK YOU</h1>
+        <p>We've added your card details</p>
+        <button type="button">Continue</button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import CardComponent from "@/components/CardComponent";
+import dayjs from "dayjs";
+import localizeFormat from "dayjs/plugin/localizedFormat"
 
 export default {
   name: "FormComponent",
   data() {
     return {
-      full_name: "",
-      MM: "",
-      YY: "",
-      CVC: ""
+      card_details: {
+        full_name: "",
+        MM: "",
+        YY: "",
+        CVC: "",
+      },
+      errors: {
+        nameError: false,
+        cardNumberError: false,
+        dateError: false,
+        CVCError: false
+      },
+      validInput: true
     }
   },
   components: {
     CardComponent
+  },
+  computed: {},
+  methods: {
+    validation(){
+      dayjs.extend(localizeFormat)
+      //const currentMonth = dayjs.format("MM");
+      const currentYear =parseInt(dayjs.format('YY'));
+      //let getMonth =(int) this.card_details.MM;
+      let getYear = this.card_details.YY;
+      if(getYear<currentYear)
+      {
+        this.errors.dateError=true;
+      }
+
+    },
   }
 }
 </script>
@@ -78,7 +114,8 @@ label {
   font-family: var(--font-fam);
   font-size: 13px;
 }
-.labels{
+
+.labels {
   display: grid;
   grid-template-columns: 2fr 3fr;
   gap: 3rem;
@@ -94,9 +131,17 @@ input {
   padding: .8rem 0 .8rem .5rem;
   width: 100%;
 }
+input[type=number]::-webkit-inner-spin-button,
+input[type=number]::-webkit-outer-spin-button {
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+}
 
 input:focus {
   color: var(--very-dark-violet);
+  border: 1px solid var(--very-dark-violet);
+  outline: none;
 }
 
 .input-wrapper {
@@ -116,16 +161,42 @@ button {
   width: 100%;
   padding: 1rem;
 }
-@media screen and (min-width: 900px){
- .page{
-   display: grid;
-   grid-template-columns: 1fr 3fr;
-   height: 100vh;
- }
-  form{
+
+.error {
+  border-color: var(--red);
+}
+
+.complete-state{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 50px;
+  max-height: fit-content;
+  padding: 1rem;
+  font-family: var(--font-fam);
+}
+
+.complete-state > h1 {
+  color: var(--very-dark-violet);
+  font-weight: 500;
+}
+.complete-state > p {
+  color:var(--light-graysh-violet);
+}
+
+@media screen and (min-width: 900px) {
+  .page {
+    display: grid;
+    grid-template-columns: 1fr 3fr;
+    height: 100vh;
+  }
+
+  form {
     max-width: 10cm;
   }
-  .form-wrapper{
+
+  .form-wrapper {
     width: 100%;
     height: 100%;
     display: grid;
